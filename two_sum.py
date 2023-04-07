d8cb3ea08ab67d4c7fa47083aa1d3900bc0ee2c6
phogan5 <philiphogan10@gmail.com>
2023-04-07 17:06:30 -0400

    two sum final

--- two_sum.py
+++ two_sum.py
@@ -1,16 +1,23 @@
-class Solution:
-    def twoSum(nums:list, target: int):
-        
-        hashset = {}
-        for i in range(len(nums)):
-            if target - nums[i] in hashset.keys():
-                print(i)
-                return [i,hashset[target-nums[i]]]
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-            else:
-                hashset[nums[i]]=i
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+'''
+def twoSum(nums: list[int], target: int) -> list[int]:
+    hashmap = {}
+    for i, n in enumerate(nums): #value:index
+        diff = target - n
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[n] = i
+
             
 
-    
-    result = twoSum(nums=[3,2, 3], target=6)
-    print(result)
+
+
+
+response = twoSum(nums=[3,2,3], target=6)
+
+print(response)
